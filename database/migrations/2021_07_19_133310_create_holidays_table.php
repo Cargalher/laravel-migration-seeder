e91559c2cb1f0e9ef43b0d2868fd3b1ec73bcef1
Cargalher <cargalher@gmail.com>
2021-07-19 15:50:15 +0200

    table created and migrated

--- database/migrations/2021_07_19_133310_create_holidays_table.php
+++ database/migrations/2021_07_19_133310_create_holidays_table.php
@@ -3,7 +3,16 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+/*
+place
+hotel_name
+date
+image_url
+children
+pets
+all_inclusive
 
+*/
 class CreateHolidaysTable extends Migration
 {
     /**
@@ -15,6 +24,13 @@ class CreateHolidaysTable extends Migration
     {
         Schema::create('holidays', function (Blueprint $table) {
             $table->id();
+            $table->string('place');
+            $table->string('hotel_name');
+            $table->string('date');
+            $table->string('image_url');
+            $table->boolean('children');
+            $table->boolean('pets');
+            $table->boolean('all_inclusive');
             $table->timestamps();
         });
     }
